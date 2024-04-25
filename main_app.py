c13fa458234738cc30e73a69c37072230f20307f
JorgeNegrete87 <knotlasher1138@gmail.com>
2024-04-24 21:28:23 -0600

    Primera rama
    Co-authored-by: Diego Paniagua <DPM2310@users.noreply.github.com>

--- main_app.py
+++ main_app.py
@@ -2,16 +2,40 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import os
+import base64
+import requests
 
-from read_image import read_image
+from read_image import read_im
+from PIL import Image
+from typing import List, Dict
 
 """ 
-# 
+# xxx
 
  ¡Bienvenido al Dermo-metro! 
 
- El cancer de piel es una enfermedad 
+Cada año, 16,000 mexicanos se enteran que tienen algún tipo de
+cancer de piel. 
+Sus principales causas incluyen exposición a radiación UV, 
+desde largas jornadas bajo el sol como exposición a 
 
 """ 
 
-read_image()
+read_im()
+
+st.header('Imagenes de Ejemplo')
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    img1_path = os.path.join(os.path.dirname(__file__), "images/melanoma.jpg")
+    st.image(img1_path)
+    img1_button = st.button("Score Image", key='1')
+with col2:
+    img2_path = os.path.join(os.path.dirname(__file__), "images/skinc.jpeg")
+    st.image(img2_path)
+    img2_button = st.button("Score Image", key='2')
+with col3:
+    img3_path = os.path.join(os.path.dirname(__file__), "images/tomato_leafspot")
+    st.image(img3_path)
+    img3_button = st.button("Score Image", key='3')
